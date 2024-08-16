5a99bddfb307eecbe15a31b83c0d9b964332899e
Andrey Trefilov <andrey.trefilov@ramax.ru>
2024-08-16 14:07:32 +0300

    publish photo to Telegram channel.

--- telegram_bot.py
+++ telegram_bot.py
@@ -8,3 +8,5 @@ load_dotenv()
 token = os.environ['BOT_API']
 bot = telegram.Bot(token=token)
 print(bot.get_me())
+#bot.send_message(text='Hi John!', chat_id="@dvmn_nasa_test")
+bot.send_document(chat_id="@dvmn_nasa_test", document=open('apod/nasa_apod_0.jpg', 'rb'))
